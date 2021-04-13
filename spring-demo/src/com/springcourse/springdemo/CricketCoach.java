0d07ce9a159e52a7effb1efe34405515d1a73c51
Johnty Simon <johntysimon90@gmail.com>
2021-04-13 21:37:05 +0530

    Covered till Spring IOC

---
+++ spring-demo/src/com/springcourse/springdemo/CricketCoach.java
@@ -0,0 +1,10 @@
+package com.springcourse.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice leg spin for 2 hrs today!";
+	}
+
+}
---
+++ spring-demo/src/com/springcourse/springdemo/MyApp.java
@@ -0,0 +1,15 @@
+package com.springcourse.springdemo;
+
+public class MyApp {
+	public static void main(String[] args) {
+
+// 		Plain Java Implementation		
+		Coach coach = new TrackCoach();
+		System.out.println(coach.getDailyWorkout());
+		
+		
+		
+		
+		
+	}
+}
