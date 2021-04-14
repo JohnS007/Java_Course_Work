334c1003bf1f3062bf8d4e6dea5313de5b3d34b1
Johnty Simon <johntysimon90@gmail.com>
2021-04-14 17:34:04 +0530

    Complete Dependency Injection

--- spring-demo/src/com/springcourse/springdemo/CricketCoach.java
+++ spring-demo/src/com/springcourse/springdemo/CricketCoach.java
@@ -1,6 +1,42 @@
 package com.springcourse.springdemo;
 
 public class CricketCoach implements Coach {
+	
+	
+	private FortuneService fortuneService;
+	
+	//add new fields team and email
+	
+	private String team;
+	private String emailAddress;
+	
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+		System.out.println("CricketCoach: Inside setter method for team");
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+		System.out.println("CricketCoach: Inside setter method for emailAddress");
+	}
+
+	public CricketCoach() {
+		super();
+		System.out.println("CricketCoach: In no-arg constructor");
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+		System.out.println("CricketCoach: Using setter method: setFortuneService() to inject dependencies");
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -9,8 +45,7 @@ public class CricketCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
