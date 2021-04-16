aca8ce6ff5b241872ea8e9eebd0c530413a202f7
Johnty Simon <johntysimon90@gmail.com>
2021-04-16 20:49:26 +0530

    Added Constructor injection using Annotations

--- spring-demo-annotations/src/com/project/springdemo/BadmintonCoach.java
+++ spring-demo-annotations/src/com/project/springdemo/BadmintonCoach.java
@@ -1,13 +1,26 @@
 package com.project.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BadmintonCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public BadmintonCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand and smash today!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
--- spring-demo-annotations/src/com/project/springdemo/SpringAnnotationDemoApp.java
+++ spring-demo-annotations/src/com/project/springdemo/SpringAnnotationDemoApp.java
@@ -12,6 +12,8 @@ public class SpringAnnotationDemoApp {
 		
 		System.out.println(theCoach.getDailyWorkout());
 		
+		System.out.println(theCoach.getDailyFortune());
+		
 		context.close();
 	}
 
