5d6cf543412c41695ee2622611e0b701a8fdb6f6
Johnty Simon <johntysimon90@gmail.com>
2021-04-16 21:29:52 +0530

    Add Qualifier for the injection

--- spring-demo-annotations/src/com/project/springdemo/BadmintonCoach.java
+++ spring-demo-annotations/src/com/project/springdemo/BadmintonCoach.java
@@ -1,12 +1,14 @@
 package com.project.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BadmintonCoach implements Coach {
 
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 //	@Autowired
--- spring-demo-annotations/src/com/project/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/project/springdemo/TennisCoach.java
@@ -1,17 +1,20 @@
 package com.project.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
-	private FortuneService fortuneService;
 	
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-	}
+	@Qualifier("happyFortuneService")
+	private FortuneService fortuneService;
+	
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
 	
 	@Override
 	public String getDailyWorkout() {
